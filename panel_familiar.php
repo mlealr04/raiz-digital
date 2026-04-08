95332d639948771d7086ac332587c81c459aca03
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-07 18:39:26 -0600

    modificaciones

--- panel_familiar.php
+++ panel_familiar.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['id_usuario'])) {
-    echo json_encode(["error" => "No sesión"]);
+    echo json_encode([]);
     exit();
 }
 
@@ -11,21 +11,16 @@ $id_usuario = $_SESSION['id_usuario'];
 $conexion = new mysqli("localhost", "root", "", "raizdigital");
 
 if ($conexion->connect_error) {
-    echo json_encode(["error" => "Error conexión"]);
+    echo json_encode([]);
     exit();
 }
 
-//  JOIN para obtener TODOS los residentes del familiar
 $sql = "SELECT r.id_residente, r.nombre 
         FROM residentes r
         INNER JOIN familiares f ON r.id_familiar = f.id_familiar
         WHERE f.id_usuario = '$id_usuario'";
 
 $result = $conexion->query($sql);
-
-if (!$result) {
-    die("Error SQL: " . $conexion->error);
-}
 
 $data = [];
 
