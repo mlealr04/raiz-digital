0d31f26998a0ed3ae2694d5557463df0f7f6a188
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-11 15:44:01 -0600

    prueba 2

--- panel_familiar.php
+++ panel_familiar.php
@@ -1,30 +1,13 @@
 <?php
 session_start();
 
-echo "ID USUARIO SESSION: " . $_SESSION['id_usuario'] . "<br>";
-
-$conexion = new mysqli("localhost", "root", "", "raizdigital");
-
-$id_usuario = $_SESSION['id_usuario'];
-
-$sql = "SELECT * FROM familiares WHERE id_usuario = '$id_usuario'";
-$result = $conexion->query($sql);
-
-if ($result->num_rows > 0) {
-    $f = $result->fetch_assoc();
-    echo "ID FAMILIAR: " . $f['id_familiar'];
-} else {
-    echo "NO EXISTE EN FAMILIARES";
-}
-
-die();
-
 $conexion = new mysqli("localhost", "root", "", "raizdigital");
 
 if ($_SESSION['rol'] != "familiar") {
     die("Acceso no autorizado");
 }
-
+echo $_SESSION['id_usuario'];
+die();
 $id_usuario = $_SESSION['id_usuario'];
 
 //  obtener id_familiar
