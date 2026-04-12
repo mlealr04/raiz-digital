7cfc8f732038936bd563ff252ae9458afd7bf031
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-11 18:41:36 -0600

    historico familiar

--- panel_familiar.php
+++ panel_familiar.php
@@ -1,22 +1,30 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['id_usuario'])) {
+    die(" No hay sesión");
+}
+
 $conexion = new mysqli("localhost", "root", "", "raizdigital");
 
 $id_usuario = $_SESSION['id_usuario'];
 
-// obtener id_familiar
-$sql_familiar = "SELECT id_familiar FROM familiares WHERE id_usuario = '$id_usuario'";
+//  buscar familiar
+$sql_familiar = "SELECT * FROM familiares WHERE id_usuario = '$id_usuario'";
 $result = $conexion->query($sql_familiar);
+
+if ($result->num_rows == 0) {
+    die("❌ Este usuario no tiene familiar registrado");
+}
+
+$familiar = $result->fetch_assoc();
 $id_familiar = $familiar['id_familiar'];
 
-// obtener residentes
+//  buscar residentes
 $sql_residentes = "SELECT * FROM residentes WHERE id_familiar = '$id_familiar'";
 $residentes = $conexion->query($sql_residentes);
 
-// mandar a la vista
+//  enviar a vista
 include("views/panel_familiar_view.php");
 ?>
-
-
 
