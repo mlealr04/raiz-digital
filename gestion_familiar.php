da7a5d32ba8c81099f602a28534df62046eb7595
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-11 19:30:01 -0600

    prueba de gestion

--- gestion_familiar.php
+++ gestion_familiar.php
@@ -1,3 +1,20 @@
 <?php
-gestion_familiar.php
+session_start();
+
+if (!isset($_SESSION['id_residente'])) {
+    die(" No hay residente seleccionado");
+}
+/*
+$id_residente = $_SESSION['id_residente'];
+
+$conexion = new mysqli("localhost", "root", "", "raizdigital");
+
+//  AQUÍ ESTÁ LA CLAVE
+$sql = "SELECT * FROM gestion_familiar 
+        WHERE id_residente = '$id_residente'
+        ORDER BY fecha DESC";
+
+$result = $conexion->query($sql);
+*/
+include("views/gestion_view.php");
 ?>
