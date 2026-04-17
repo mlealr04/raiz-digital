2f5bc69f3da1405775f367138619e13a5c981833
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-17 09:30:33 -0600

    Actividades

---
+++ crear_actividad.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['id_usuario']) || !isset($_SESSION['id_residente'])) {
+    die("❌ Sesión no válida");
+}
+
+$id_enfermero = $_SESSION['id_usuario'];
+$id_residente = $_SESSION['id_residente'];
+
+// conexión
+$conexion = new mysqli("localhost", "root", "", "raizdigital");
+
+if ($conexion->connect_error) {
+    die("Error conexión");
+}
+
+// datos del form
+$titulo = $_POST['titulo'];
+$descripcion = $_POST['descripcion'];
+$fecha = $_POST['fecha'];
+$hora = $_POST['hora'];
+
+// INSERT
+$sql = "INSERT INTO actividades 
+(id_residente, id_enfermero, titulo, descripcion, fecha, hora, estado)
+VALUES 
+('$id_residente', '$id_enfermero', '$titulo', '$descripcion', '$fecha', '$hora', 'pendiente')";
+
+if ($conexion->query($sql) === TRUE) {
+    echo " Actividad creada";
+
+    // redirigir a gestión
+    header("Location: views/gestion_familiar_enfermero.php");
+    exit();
+
+} else {
+    echo "❌ Error: " . $conexion->error;
+}
+
+$conexion->close();
+?>
