f7b91a9abe11e8ffc44be851fc94319aaf2a57c6
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-19 13:48:52 -0600

    familiar

--- views/gestion_view_familiar.php
+++ views/gestion_view_familiar.php
@@ -179,7 +179,30 @@ hr {
             <?php endif; ?>
 
         </div>
+<?php
+$sqlAvisos = "SELECT * FROM avisos 
+              WHERE id_residente = '$id_residente'";
+$avisos = $conexion->query($sqlAvisos);
+?>
 
+<div class="agenda">
+    <div class="agenda-title">🔔 AVISOS</div>
+
+    <?php if ($avisos && $avisos->num_rows > 0): ?>
+        <?php while($a = $avisos->fetch_assoc()): ?>
+            <div class="agenda-item">
+                <div>
+                    <strong><?php echo $a['titulo']; ?></strong><br>
+                    <?php echo $a['descripcion']; ?><br>
+                    Cantidad: <?php echo $a['cantidad']; ?><br>
+                    Fecha: <?php echo $a['fecha']; ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php else: ?>
+        <p>No hay avisos</p>
+    <?php endif; ?>
+</div>
     </div>
 
 </div>
