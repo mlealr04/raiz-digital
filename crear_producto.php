66c3de6aecaa82c22598037b7da84d477a568d35
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-19 21:10:33 -0600

    quitar prueba

--- crear_producto.php
+++ crear_producto.php
@@ -11,9 +11,6 @@ $imagen = $_FILES['imagen']['name'];
 $esp = $_POST['especificaciones'];
 move_uploaded_file($_FILES['imagen']['tmp_name'], "uploads/" . $imagen);
 
-var_dump($id_residente);
-die();
-
 $sql = "INSERT INTO productos 
 (nombre, cantidad, tipo, imagen, especificaciones, id_residente)
 VALUES 
