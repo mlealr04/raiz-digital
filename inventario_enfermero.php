f3ada00fef6f8163f969892fffd0973032d6f3bb
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-19 19:12:02 -0600

    no quedo jeje

--- inventario_enfermero.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$conexion = new mysqli("localhost", "root", "", "raizdigital");
-
-$sql = "SELECT * FROM productos";
-$productos = $conexion->query($sql);
-
-include("views/inventario_view_enfermero.php");
