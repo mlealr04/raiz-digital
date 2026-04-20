34611832790e663b8727f42b84067e877945db01
alexiamontserrat-design <alexiamontserrat@gmail.com>
2026-04-19 21:09:12 -0600

    base de datos

--- crear_producto.php
+++ crear_producto.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+$id_residente = $_SESSION['id_residente'];
+
 $conexion = new mysqli("localhost", "root", "", "raizdigital");
 
 $nombre = $_POST['nombre'];
@@ -8,9 +11,14 @@ $imagen = $_FILES['imagen']['name'];
 $esp = $_POST['especificaciones'];
 move_uploaded_file($_FILES['imagen']['tmp_name'], "uploads/" . $imagen);
 
-$id_residente = $_SESSION['id_residente'];
-$sql = "INSERT INTO productos (nombre, cantidad, tipo, imagen, especificaciones, id_residente)
-VALUES ('$nombre', '$cantidad', '$tipo', '$imagen', '$esp', '$id_residente')";
+var_dump($id_residente);
+die();
+
+$sql = "INSERT INTO productos 
+(nombre, cantidad, tipo, imagen, especificaciones, id_residente)
+VALUES 
+('$nombre', '$cantidad', '$tipo', '$imagen', '$esp', '$id_residente')";
+
 $conexion->query($sql);
 
 header("Location: inventario_enfermero.php");
